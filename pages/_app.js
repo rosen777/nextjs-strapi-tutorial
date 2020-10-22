1fbbfdcbbef7d81e7fde713279828afde2646fab
Rosen Toshev <rosen.toshev@expend.io>
2020-10-22 19:27:32 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import React from "react";
+import Head from "next/head";
+import "../assets/css/style.css";
+import { ApolloProvider } from "@apollo/react-hooks";
+import withData from "../utils/apollo";
+import Nav from "../components/nav";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps, apollo }) => {
+  return (
+    <ApolloProvider client={apollo}>
+      <Head>
+        <Nav /> // Put the Nav component just after the Head component
+        <title>Strapi blog</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Staatliches"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css"
+        />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js" />
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js" />
+      </Head>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+};
 
-export default MyApp
+// Wraps all components in the tree with the data provider
+export default withData(App);
